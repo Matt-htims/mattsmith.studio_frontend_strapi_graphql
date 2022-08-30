0119aa9d744951ab758ff5f70e1bd6c69b056f42
Matt Smith <matt.smith3108@gmail.com>
2022-08-30 12:32:02 +0100

    Added BasicScroll to components

--- components/layout/CTAsection.jsx
+++ components/layout/CTAsection.jsx
@@ -1,12 +1,19 @@
+import BasicScroll from "../animations/basicScroll";
+
 export default function CTAsection({ cta, body, buttonText, buttonLink }) {
 	return (
 		<div className="bg-lightBlue rounded-3xl shadow-md text-center lg:pt-20 lg:pb-16 lg:px-12 pt-14 pb-10 px-8 lg:mt-32 mt-24 mb-16 max-w-screen-lg m-auto">
-			<div className="text space-y-8 mb-14">
-				<h3 className="font-spaceGrotesk font-medium md:text-4xl text-3xl text-textBlue-dark">
-					{cta}
-				</h3>
-				<p className="md:text-2xl text-lg font-light">{body}</p>
-			</div>
+			<BasicScroll
+				content={
+					<div className="text space-y-8 mb-14">
+						<h3 className="font-spaceGrotesk font-medium md:text-4xl text-3xl text-textBlue-dark">
+							{cta}
+						</h3>
+						<p className="md:text-2xl text-lg font-light">{body}</p>
+					</div>
+				}
+			/>
+
 			<div className="button">
 				<a href={buttonLink}>
 					<button className="bg-ctaOrange hover:bg-textBlue px-6 py-3 rounded-xl shadow-lg font-spaceGrotesk text-offWhite text-2xl hover:cursor-pointer transition-all">
--- components/layout/TripleExplainer.jsx
+++ components/layout/TripleExplainer.jsx
@@ -6,39 +6,42 @@ import BasicScroll from "../animations/basicScroll";
 export default function TripleExplainer({ title, contentArray }) {
 	return (
 		<div className="bg-lightBlue px-8 lg:py-20 pt-16 pb-4 lg:mt-26 mt-24 rounded-3xl drop-shadow-md">
-			<BasicScroll
-				content={
-					<h2 className="font-spaceGrotesk font-medium md:text-5xl text-4xl text-center md:mb-24 mb-16 text-textBlue-dark">
-						{title}
-					</h2>
-				}
-			/>
-
+			<h2 className="font-spaceGrotesk font-medium md:text-5xl text-4xl text-center md:mb-24 mb-16 text-textBlue-dark">
+				{title}
+			</h2>
 			<div className="content lg:flex justify-around items-center lg:space-y-0 space-y-16">
 				{contentArray &&
 					contentArray.map((content) => (
-						<div
-							key={content.id}
-							className="text-center lg:border-b-0 border-b-2 border-[#DEE1ED] last:border-b-0 lg:pb-0 pb-16"
-						>
-							<div className="image mb-8 flex justify-center">
-								<Image data={content.icon.responsiveImage} alt={title} />
-							</div>
-							<div className="flex flex-col justify-between h-64">
-								<h2 className="font-spaceGrotesk font-medium text-2xl">
-									{content.title}
-								</h2>
-								<p className="font-light lg:w-10/12 m-auto pb-6">
-									{content.copy}
-								</p>
-								<div className="space-y-2">
-									<p className="text-textBlue-light font-light text-base">
-										{content.bottomText}
-									</p>
-									<p className="font-medium text-lg">{content.bottomText2}</p>
-								</div>
-							</div>
-						</div>
+						<>
+							<BasicScroll
+								content={
+									<div
+										key={content.id}
+										className="text-center lg:border-b-0 border-b-2 border-[#DEE1ED] last:border-b-0 lg:pb-0 pb-16"
+									>
+										<div className="image mb-8 flex justify-center">
+											<Image data={content.icon.responsiveImage} alt={title} />
+										</div>
+										<div className="flex flex-col justify-between h-64">
+											<h2 className="font-spaceGrotesk font-medium text-2xl">
+												{content.title}
+											</h2>
+											<p className="font-light lg:w-10/12 m-auto pb-6">
+												{content.copy}
+											</p>
+											<div className="space-y-2">
+												<p className="text-textBlue-light font-light text-base">
+													{content.bottomText}
+												</p>
+												<p className="font-medium text-lg">
+													{content.bottomText2}
+												</p>
+											</div>
+										</div>
+									</div>
+								}
+							/>
+						</>
 					))}
 			</div>
 		</div>
